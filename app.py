ea0f44cd99ca491d8ced61638252f4f5c4df26c5
bigfacilityiisr <bigfacilityiisr@gmail.com>
2024-10-19 14:00:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,67 @@
+import streamlit as st
+import joblib
+from Bio.SeqUtils.ProtParam import ProteinAnalysis
+import numpy as np
+import pickle
+
+# Load your trained model
+model = joblib.load ('Best Model.pkl')
+top_n_features = np.load('top_n_features.npy')
+
+def calculate_aac(sequence):
+    protein_analysis = ProteinAnalysis(sequence)
+    aac = protein_analysis.get_amino_acids_percent()
+    return aac
+
+def calculate_k_spaced_pairs(sequence, k):
+    pairs = {}
+    length = len(sequence)
+    for i in range(length - k):
+        pair = sequence[i] + sequence[i + k]
+        pairs[pair] = pairs.get(pair, 0) + 1
+    return pairs
+
+def calculate_pse_aac_type_ii(sequence, k, max_length):
+    aac = calculate_aac(sequence)
+    pairs = calculate_k_spaced_pairs(sequence, k)
+    dipeptides = [sequence[i:i+2] for i in range(len(sequence)-1)]
+    dipeptide_counts = {dipeptide: dipeptides.count(dipeptide) for dipeptide in set(dipeptides)}
+    pse_aac = list(aac.values())
+    pse_aac.extend(pairs.values())
+    pse_aac.extend(dipeptide_counts.values())
+    if len(pse_aac) < max_length:
+        pse_aac.extend([0] * (max_length - len(pse_aac)))
+    elif len(pse_aac) > max_length:
+        pse_aac = pse_aac[:max_length]
+    return np.array(pse_aac)
+
+# Preprocess a single input sequence
+def preprocess_sequence(sequence1, sequence2, k=3, max_length=3601):
+    pse_aac_1 = calculate_pse_aac_type_ii(sequence1, k, max_length)
+    pse_aac_2 = calculate_pse_aac_type_ii(sequence2, k, max_length)
+    combined_features = np.concatenate((pse_aac_1, pse_aac_2))
+    return combined_features
+
+# Predict function for a single input
+def predict_interaction(sequence1, sequence2):
+    # Preprocess the input sequences
+    input_features = preprocess_sequence(sequence1, sequence2)
+    input_features = input_features.reshape(1, -1)
+    input_features_s = input_features[:, top_n_features] 
+    
+    prediction = model.predict(input_features_s)
+    prediction_proba = model.predict_proba(input_features_s)
+    
+    return prediction, prediction_proba # Reshape for a single sample
+
+    # Select top N features
+
+st.sidebar.title("About")
+st.sidebar.write("""
+This application allows you to input protein sequences and predict their interaction.
+
+""")   
+
 st.title('Protein Interaction Predictor')
 
 st.write("""
